879ea416be9db59ce32af89640f675a1cd3cdbe9
Seyed Amir Mehrizi <s.a.mehrizi1989@gmail.com>
2022-10-05 13:07:50 +0330

    add logo and filters to the sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,9 +1,19 @@
-import './sidebar.css'
-
+import './sidebar.css';
+import Logo from '../../assets/images/logo.jpg';
 function Sidebar() {
     return (
         <div className='sidebar'>
-            Sidebar
+            <div className='sidebar-wrapper'> 
+                <div className='logo-container'>
+                    <img src={Logo} alt='EverSport' className='logo' />
+                </div>
+                <div className='filter-info-container'>
+                    <span className='filter-counter'>1 Filter</span>
+                    <span className='price-range-filter'>from 10 euros • 2 & up</span>
+                    <span className='clear-all'>Clear all</span>
+                </div>
+
+            </div>
         </div>
     )
 }
